ee8085f4a90cab3e2b38ba6d37a27e62210a448c
Aaron Deas <deas.aaron@gmail.com>
2022-03-16 18:27:14 -0400

    Initial Commit

---
+++ pages/login.tsx
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+const login = () => {
+  return (
+    <Layout>
+      <div>
+        <h1>Login</h1>
+      </div>
+    </Layout>
+  );
+};
+
+export default login;
