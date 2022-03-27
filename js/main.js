c75f29cafb9f59b2ff79d47875ba8565868e446d
Aaron Deas <deas.aaron@gmail.com>
2022-03-27 02:42:48 -0400

    Got bones of site worked out

--- js/main.js
+++ js/main.js
@@ -1,27 +1,39 @@
 let currentTime = document.querySelector('.currentTime');
 let userData = {
-    resources: 1,
-    resourcesPerClick: 1
+  resources: 1,
+  resourcesPerClick: 1
 };
 
 (() => {
-    setInterval(() => {
-        currentTime.innerHTML = new Date().toLocaleTimeString();
-    }, 1000);
-    
+  setInterval(() => {
+    currentTime.innerHTML = new Date().toLocaleTimeString();
+  }, 1000);
 
-    if (localStorage.getItem('userData')) {
-        userData = JSON.parse(localStorage.getItem('userData'));
-    } else {
-        let formDiv = document.querySelector('.formDiv');
-        let userForm = document.createElement('form');
-        let nameInput = document.createElement('input');
-        userForm.classList.add('userForm');
-        nameInput.setAttribute('type', 'text');
-        nameInput.setAttribute('placeholder', 'Enter your name');
-        nameInput.setAttribute('name', 'userName');
-        userForm.appendChild(nameInput);
-        formDiv.appendChild(userForm);
-        
+  const getUserName = () => {
+    userData.userName = document.querySelector('.nameInput');
+    let pEl = document.createElement('p');
+    let infoDivTop = document.querySelector('.infoDivTop');
+    pEl.innerHTML = `Welcome ${
+      JSON.parse(localStorage.getItem('userData')).userName
     }
+    <br>    
+    You have just started your first Asteroid mining company!
+    `;
+    infoDivTop.insertBefore(pEl, infoDivTop.firstChild);
+  };
+
+  if (localStorage.getItem('userData')) {
+    userData = JSON.parse(localStorage.getItem('userData'));
+    getUserName();
+  } else {
+    let userNameForm = document.querySelector('.userNameForm');
+    userNameForm.setAttribute('style', 'display: block');
+    userNameForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      userData.userName = e.target.userName.value;
+      localStorage.setItem('userData', JSON.stringify(userData));
+      userNameForm.style.display = 'none';
+      document.querySelector('.userName').innerHTML = userData.userName;
+    });
+  }
 })();
