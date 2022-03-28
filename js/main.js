87b798dd24d86b961fded442368400f75987cc5c
Aaron Deas <deas.aaron@gmail.com>
2022-03-28 04:31:13 -0400

    Added buttons and new grid to main section of page

--- js/main.js
+++ js/main.js
@@ -1,14 +1,30 @@
 let currentTime = document.querySelector('.currentTime');
 let userData = {
+  mining: false,
   resources: 1,
   resourcesPerClick: 1
 };
+const resourceGeneratorButton = document.querySelector(
+  '.resourceGeneratorButton'
+);
 
 (() => {
   setInterval(() => {
     currentTime.innerHTML = new Date().toLocaleTimeString();
   }, 1000);
 
+  resourceGeneratorButton.addEventListener('click', () => {
+    userData.resources += userData.resourcesPerClick;
+    if (userData.mining) {
+      userData.mining = false;
+    } else {
+      userData.mining = true;
+    }
+    if (userData.mining) {
+      setInterval(updateResources, 1000);
+      console.log(userData);
+    }
+  });
   const getUserName = () => {
     userData.userName = document.querySelector('.nameInput');
     let pEl = document.createElement('p');
@@ -27,13 +43,20 @@ let userData = {
     getUserName();
   } else {
     let userNameForm = document.querySelector('.userNameForm');
+    let userNameSubmitBtn = document.querySelector('.userNameSubmitBtn');
     userNameForm.setAttribute('style', 'display: block');
     userNameForm.addEventListener('submit', (e) => {
-      e.preventDefault();
       userData.userName = e.target.userName.value;
       localStorage.setItem('userData', JSON.stringify(userData));
       userNameForm.style.display = 'none';
+      userNameSubmitBtn.onclick = getUserName;
       document.querySelector('.userName').innerHTML = userData.userName;
     });
   }
+
+  const updateResources = () => {
+    let resourceAmt = document.querySelector('.resourceAmt');
+    userData.resources += userData.resourcesPerClick;
+    resourceAmt.innerHTML = userData.resources;
+  };
 })();
