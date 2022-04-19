50f34f9af2aae64a53ba3aeaf6453e6881d9b3a5
Aaron Deas <deas.aaron@gmail.com>
2022-04-19 01:24:52 -0400

    Saving progress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,34 @@ import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [resourcesState, setResourcesState] = useState({
-    resources: 0
+    resources: 0,
+    mining: false
   });
+
   const handleClick = () => {
-    setResourcesState({ resources: resourcesState.resources + 1 });
-    console.log(resourcesState)
+    // let resourcesInterval;
+    // if (resourcesState.mining) {
+    //   setResourcesState({
+    //     ...resourcesState,
+    //     mining: false
+    //   });
+    //   clearInterval(resourcesInterval);
+    // } else {
+    //   setResourcesState({
+    //     ...resourcesState,
+    //     mining: true
+    //   });
+    //   resourcesInterval = setInterval(updateResources, 1000);
+    // }
+    updateResources();   
+  };
+
+  const updateResources= () => {
+    setResourcesState({
+      ...resourcesState,
+      resources: resourcesState.resources + 1
+    });
+    console.log('update resources', resourcesState)
   };
   return (    
       <Component {...pageProps}  resources={resourcesState.resources} handleClick={handleClick}/>    
